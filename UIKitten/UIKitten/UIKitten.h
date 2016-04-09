c080762b04d76a6d499e7f96559e8d62d5fe1163
Carsten Könemann <cargath@gmail.com>
2016-04-10 01:43:03 +0200

    initialized project

---
+++ UIKitten/UIKitten/UIKitten.h
@@ -0,0 +1,19 @@
+//
+//  UIKitten.h
+//  UIKitten
+//
+//  Created by Carsten Könemann on 10.04.16.
+//  Copyright © 2016 cargath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitten.
+FOUNDATION_EXPORT double UIKittenVersionNumber;
+
+//! Project version string for UIKitten.
+FOUNDATION_EXPORT const unsigned char UIKittenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitten/PublicHeader.h>
+
+
